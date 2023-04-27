b1ee3d895192928629d116eaef0a79a33630b5c3
BL4Z3_Kanazaki <74963436+1948dj942@users.noreply.github.com>
2023-04-27 09:19:09 +0700

    Add files via upload

---
+++ Create data .py
@@ -0,0 +1,37 @@
+import cv2
+import os
+import numpy as np
+detector=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+id=1
+if id == 1:
+    print(0)
+    for i in range(1,6):
+        for j in range (1,10):
+            filename = 'data\image'  + str(i) + '.' +str(j) + '.jpg'
+            frame = cv2.imread(filename)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            fa = detector.detectMultiScale(gray, 1.1, 5)
+            for(x,y,w,h) in fa:
+                cv2.rectangle(frame,(x,y),(x+w, y+h),(0,255,0), 2)
+                if not os.path.exists('dataset'):
+                    os.makedirs('dataset')
+                cv2.imwrite('dataset/image'  + str(i) + '.' +str(j) + '.jpg', gray[y:y+h,x:x+w])
+if id == 2:
+    cap = cv2.VideoCapture(0)
+    detector=cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+    sampleNum = 0
+    while(True):
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        fa = detector.detectMultiScale(gray, 1.1, 5)
+        for(x,y,w,h) in fa:
+            cv2.rectangle(frame,(x,y),(x+w, y+h),(0,255,0), 2)
+            if not os.path.exists('dataset2'):
+                os.makedirs('dataset2')
+            sampleNum+=1
+            cv2.imwrite('dataset2/image'+str(1)+'.'+str(sampleNum)+'.jpg', gray[y:y+h,x:x+w])
+        cv2.imshow('frame',frame)
+        if sampleNum > 19:
+            break;
+    cap.release()
+    cv2.destroyAllWindows()
